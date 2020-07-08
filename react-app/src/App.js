b98ea57756cd00bf82d8ea89d0f2202505849685
harrymkwn <heratmakwana19@gmail.com>
2020-07-08 19:15:25 +0530

    Upload feature added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state = {
+      seletedFile: null
+    }
+  }
+  onClickHandler = () => {
+    const data = new FormData() 
+    data.append('file', this.state.selectedFile);
+    axios.post("http://localhost:8000/upload", data, { /* receive two parameter endpoint url ,form data*/})
+    .then(res => { // then print response status
+      console.log(res.statusText)
+    });
+  };
+
+  onChangeHandler=event=>{
+    console.log(event.target.files[0]);
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <form method="post" action="#" id="#">
+                <div className="form-group files">
+                  <label> Upload Your files</label>
+                  <input type="file" name="file" className="form-control" onChange={this.onChangeHandler}/>
+                  <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button> 
+                </div>
+              </form>
+            </div>
+
+            <div className="col-md-6">
+              <form method="post" action="#" id="#">
+                <div className="form-group files color">
+                  <label >Upload Your file</label>
+                  <input type="file" className="form-control" multiple=""/>
+                </div>
+              </form>
+
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
